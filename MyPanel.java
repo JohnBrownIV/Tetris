e7bd708c2fddf3ffaa7774f5751609e25cf05c5d
John Brown <100054328@csisd.org>
2024-09-18 14:54:09 -0500

    idk

--- MyPanel.java
+++ MyPanel.java
@@ -10,6 +10,7 @@ int[][] boardStatus;
 int width;
 int height;
 int gridSize;
+ArrayList<Coord> activePeices;
  
   MyPanel(){
 
@@ -24,6 +25,7 @@ int gridSize;
     height = 900;
     boardStatus = new int[10][20];
     gridSize = (height - 100) / 20;
+    boardStatus[2][3] = 1;
   }
  
   public void paint(Graphics g) {
@@ -35,10 +37,22 @@ int gridSize;
     g2D.setPaint(Color.gray);
     g2D.fillRect(0, 0, width, 100);
     g2D.fillRect(0, 0, 200, height);
-    g2D.setPaint(Color.white);
+    g2D.fillRect(width - 200, 0, 200, height);
     for (int x = 0; x < 20; x++) {
       for (int i = 0; i < 10; i++) {
-        g2D.drawRect(200 + gridSize * i), 100 + (x * ((height - 100) / 20)), ((height - 100) / 20), ((height - 100) / 20));
+        switch (boardStatus[i][x]) {
+          case 0:
+            g2D.setPaint(Color.white);
+            g2D.drawRect(200 + (gridSize * i), 100 + (x * gridSize), gridSize, gridSize);
+            break;
+          case 1:
+            g2D.setPaint(Color.red);
+            g2D.fillRect(200 + (gridSize * i), 100 + (x * gridSize), gridSize, gridSize);
+            g2D.setPaint(Color.black);
+            g2D.drawRect(200 + (gridSize * i), 100 + (x * gridSize), gridSize, gridSize);
+            break;
+          default:
+        }
       }
     }
 
